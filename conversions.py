7dc7eb22bc8deddd4433da435a771e6d3f5bc23c
Gabriel Cánepa <gacanepa@gmail.com>
2023-05-20 10:19:56 -0300

    Implemented binary to decimal conversion

---
+++ conversions.py
@@ -0,0 +1,15 @@
+from exceptions import BinaryToDecimalError
+from valid_characters import BINARY_CHARACTERS
+
+
+def from_binary_to_decimal(value: str) -> int:
+    if not isinstance(value, str):
+        raise TypeError(f"{value} is not a string")
+    if not all([character in BINARY_CHARACTERS for character in value]):
+        raise BinaryToDecimalError(f"{value} contains non-binary characters")
+    characters = list(value)
+    result = 0
+    for index, character in enumerate(characters):
+        result += int(character) * 2 ** (len(characters) - int(index) - 1)
+
+    return result
