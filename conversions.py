27e50c8eaf6b40c3b6d3cdc49a4597277257f47b
Gabriel Cánepa <gacanepa@gmail.com>
2023-06-03 16:32:11 -0300

    Create function to convert from decimal to binary

--- conversions.py
+++ conversions.py
@@ -1,3 +1,4 @@
+from collections import deque
 from exceptions import ConversionError
 from valid_characters import BINARY_CHARACTERS
 
@@ -13,3 +14,23 @@ def from_binary_to_decimal(value: str) -> int:
         result += int(character) * 2 ** (len(characters) - int(index) - 1)
 
     return result
+
+
+def from_decimal_to_binary(value: str) -> str:
+    if not value.isdigit():
+        raise TypeError(f"{value} is not an integer number")
+    rest_value = int(value)
+    # The problem domain does not require converting negative numbers
+    if rest_value < 0:
+        raise TypeError(f"{value} should be an integer greater than 0")
+    if rest_value in [0, 1]:
+        return value
+    # Use a deque to append items to the left for performance reasons
+    result = deque([])
+    while rest_value > 1:
+        rest_value, remainder = divmod(rest_value, 2)
+        result.appendleft(str(remainder))
+    # Append the last remainder (less significant bit) once outside the loop
+    result.appendleft(str(remainder))
+
+    return "".join(result)
